2b630b4e636afa05862654525519cf57370977f7
Muhammad Manzoor Murshed <mmmftcctgbdpuc@gmail.com>
2026-07-27 09:07:38 +0600

    feat: protect sensitive property routes with token verification middleware

--- src/routes/propertyRoutes.js
+++ src/routes/propertyRoutes.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const { addProperty, getFeaturedProperties, getProperties, getMyProperties, updateMyProperty, deleteMyProperty, getPropertyDetails, addReview, getReviews, getMyRatings } = require("./../controllers/propertyController");
+const verifyFirebaseToken = require("../middlewares/verifyFirebaseToken");
 
-router.post("/add-properties", addProperty);
+router.post("/add-properties", verifyFirebaseToken, addProperty);
 router.get("/get-featured-properties", getFeaturedProperties);
 router.get("/get-properties", getProperties);
-router.get("/get-my-properties", getMyProperties);
-router.get("/get-property-details/:id", getPropertyDetails);
-router.patch("/update-my-property/:id", updateMyProperty);
-router.delete("/delete-my-property/:id", deleteMyProperty);
+router.get("/get-my-properties", verifyFirebaseToken, getMyProperties);
+router.get("/get-property-details/:id", verifyFirebaseToken, getPropertyDetails);
+router.patch("/update-my-property/:id", verifyFirebaseToken, updateMyProperty);
+router.delete("/delete-my-property/:id", verifyFirebaseToken, deleteMyProperty);
 router.post("/add-review", addReview);
 router.get("/get-reviews/:id", getReviews);
-router.get("/get-my-ratings", getMyRatings)
+router.get("/get-my-ratings", verifyFirebaseToken, getMyRatings)
 
 module.exports = router;
